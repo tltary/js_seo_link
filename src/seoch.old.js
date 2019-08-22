a81979debaa40c2225419c295e2c79e026e698e3
Antony Pyatkov <tltary@yandex.ru>
2019-08-22 17:27:56 +0400

    Update seoch.old.js

--- src/seoch.old.js
+++ src/seoch.old.js
@@ -1,20 +1,20 @@
 function seoCh(el) {
 	var link = document.querySelectorAll(''+el+'');
 
-	for (var i=0;i<link.length;i=i+1) {
-		link[i].addEventListener('click', actionLink, false);
-	};
-	delete i;
-
 	function actionLink(e) {
 		var target = e.target.dataset.target;
 		var href   = e.target.dataset.href;
 		if (target === '_blank') {
 			window.open(href,target)
+			delete target;
+			delete href;
 		} else {
 			location.assign(href)
 		};
-		delete target;
-		delete href;
 	}
+
+	for (var i=0;i<link.length;i=i+1) {
+		link[i].addEventListener('click', actionLink, false);
+	};
+	delete i;
 }
